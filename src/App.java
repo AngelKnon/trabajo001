b921426635a5fab0f7f27ad6841af1dab3131874
AngelKnon <miguecanon03@gmail.com>
2021-07-01 19:16:18 -0500

    Second class, java coding

--- src/App.java
+++ src/App.java
@@ -1,4 +1,11 @@
 public class App {
+
+private String nombre;
+private int edad;
+private char sexo;
+private double estatura;
+private float peso;
+private boolean estadoCivil;
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
